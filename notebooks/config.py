29048fded03aa890f95adfc956e611f099c0efd3
ayushkumar <ge95xod@mytum.de>
2024-02-05 16:36:52 +0100

    Performed Step 2 of our refactoring journey i.e., added paramters to be explicitly tuned in without changing any code.

---
+++ notebooks/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+def csv_data_path() -> Path:
+    """
+    Returns the location of the RTM Simulation CSV data, allowing for script executions in subfolders without worrying about the
+    relative location of the data
+
+    :return: the path to the CSV file
+    """
+    cwd = Path(".")
+    for folder in (cwd, cwd / "..", cwd / ".." / ".."):
+        data_folder = folder / "data"
+        if data_folder.exists() and data_folder.is_dir():
+            return data_folder / "RtmSimulation_kickstart.csv"
+        
